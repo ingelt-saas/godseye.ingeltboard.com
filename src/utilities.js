98868370ce8b6b813ebcb99729def7ff89f69601
Palash <palashnath880@gmail.com>
2023-09-25 17:54:01 +0600

    Discussion Page Updated

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export const bytesToSize = (bytes) => {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     if (bytes === 0) return '0 Byte';
@@ -11,4 +13,22 @@ export const getFileType = (fileName) => {
     // Get the last portion of the file name after the last dot (.)
     const fileExtension = fileName.split('.').pop();
     return fileExtension.toLowerCase(); // Convert to lowercase for consistency
+}
+
+// discussion date beautify
+export const formatDate = (date) => {
+    // date = moment(date);
+    const inputDate = moment(date, 'YYYY-MM-DD')
+    const currentDate = moment();
+
+    if (inputDate.isSame(currentDate, 'day')) {
+        return 'Today';
+    }
+
+    if (currentDate.diff(inputDate, 'days') < 7) {
+        return inputDate.format('dddd'); // Return day name
+    } else {
+        return inputDate.format('ll'); // Return full date
+    }
+
 }
